7ebcc51cb29aa765d9f40acff0101a8c080e1a74
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-11-07 09:54:41 +0500

    updated

--- portfolio-website/src/app/layout.tsx
+++ portfolio-website/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,11 +25,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
   );
