5a444770ed3b3844a0e69059aa94024a60a6d267
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-11-10 17:16:31 +0500

    updated

--- portfolio-web/src/app/contact/page.tsx
+++ portfolio-web/src/app/contact/page.tsx
@@ -3,6 +3,7 @@ import Button from "@/components/Button";
 import Headings from "@/components/Headings";
 import { ReactEventHandler, useState } from "react";
 import { FiSend } from "react-icons/fi";
+import { LuPhoneCall } from "react-icons/lu";
 import React, { ChangeEvent } from "react";
 
 export default function Contact() {
@@ -21,40 +22,42 @@ export default function Contact() {
   return (
     <div className="px-6 lg:px-12 2xl:px-24 relative">
       <Headings headText="Contact." headClass="mb-6" />
-      <div></div>
       <div>
-        <form onSubmit={handleSubmit} className="grid gap-2">
+        <div>
+          <div>
+            <LuPhoneCall />
+          </div>
+        </div>
+        <form onSubmit={handleSubmit}>
           <input
             type="text"
             placeholder="Your Name"
             required
-            className="w-full sm:w-1/2 outline-none border-[1px] border-zinc-300 bg-white px-4 py-2 text-zinc-400 text-sm rounded-md"
+            className="w-full outline-none border-[1px] border-zinc-300 bg-white px-4 py-2 my-1 focus:border-red-400 text-zinc-400 text-sm rounded-md"
             onChange={handleChange}
           />
           <input
             type="email"
             placeholder="Email Address"
             required
-            className="w-full sm:w-1/2 outline-none border-[1px] border-zinc-300 bg-white px-4 py-2 text-zinc-400 text-sm rounded-md"
+            className="w-full outline-none border-[1px] border-zinc-300 bg-white px-4 py-2 my-1 focus:border-red-400 text-zinc-400 text-sm rounded-md"
             onChange={handleChange}
           />
           <input
             type="tel"
             placeholder="Phone Number"
             required
-            className="w-full outline-none border-[1px] border-zinc-300 bg-white px-4 py-2 text-zinc-400 text-sm rounded-md"
+            className="w-full outline-none border-[1px] border-zinc-300 bg-white px-4 py-2 my-1 focus:border-red-400 text-zinc-400 text-sm rounded-md"
             onChange={handleChange}
           />
           <textarea
             placeholder="Message"
             required
-            className="w-full outline-none border-[1px] border-zinc-300 bg-white px-4 py-2 text-zinc-400 text-sm rounded-md"
+            className="w-full outline-none border-[1px] border-zinc-300 bg-white px-4 py-2 my-1 focus:border-red-400 text-zinc-400 text-sm rounded-md"
           ></textarea>
           <Button btnText="Send Message" btnClass="" btnIcon={<FiSend />} />
         </form>
       </div>
-
-      
     </div>
   );
 }
