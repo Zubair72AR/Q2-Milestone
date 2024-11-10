d83adf87dd9811a04e36a7a2f453a7cfd625a3f6
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-11-10 22:33:17 +0500

    updated

--- portfolio-web/src/components/Footer.tsx
+++ portfolio-web/src/components/Footer.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import Button from "@/components/Button";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <div>
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus unde suscipit similique a pariatur hic quibusdam illum ab assumenda commodi! Obcaecati sequi repellat illo iste reiciendis. Incidunt perferendis obcaecati praesentium?
-        </h1>
+        <div>
+            <div></div>
+            <h1>Let's Talk?</h1>
+            <div></div>
+            </div>
+            <Link href="contact">
+          <Button
+            btnText="Hire Me"
+            btnClass="px-4 py-[7px] hidden xs:flex"
+            
+          />
+        </Link>
     </div>
   )
 }
