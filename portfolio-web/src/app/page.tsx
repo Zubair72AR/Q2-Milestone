f79bfdb1f8fb2d1514aa0a9a4b48dbfec00c634c
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-11-11 17:54:50 +0500

    updated

--- portfolio-web/src/app/page.tsx
+++ portfolio-web/src/app/page.tsx
@@ -6,12 +6,14 @@ import portfolioImage from "../../public/hero_02.png";
 import Button from "@/components/Button";
 import { IoCloudDownloadOutline } from "react-icons/io5";
 import { BsBarChartLine } from "react-icons/bs";
+import { MdOutlineRocketLaunch } from "react-icons/md";
+import { AiOutlinePieChart } from "react-icons/ai";
 
 export default function Home() {
   return (
     <div className="px-6 lg:px-12 2xl:px-24 py-12">
-      <div className="flex h-[600px]">
-        <div className="px-2 relative overflow-hidden">
+      <div className="flex h-[500px]">
+        <div className="px-2 overflow-hidden relative">
           <motion.span
             initial={{ scaleX: 0, x: "-50%" }}
             animate={{ scaleX: 1 }}
@@ -50,7 +52,6 @@ export default function Home() {
           initial={{ opacity: 0, scale: 0.8, rotate: 15 }}
           animate={{ opacity: 1, scale: 1, rotate: 0 }}
           transition={{ duration: 0.8 }}
-          whileHover={{ scale: 1.1 }}
           className="flex justify-center items-center"
         >
           <Image
@@ -58,31 +59,93 @@ export default function Home() {
             alt="Portfolio Image"
             width={500}
             height={300}
-            className="w-[500px]"
+            className="w-[550px] relative -bottom-12 -z-10"
           />
         </motion.div>
       </div>
       <div className="flex justify-between items-center">
-        <div className="px-10 py-16 w-96 bg-gradient-to-br from-red-500 via-red-600 to-orange-600">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.7 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ delay: 0, duration: 0.5 }}
+          className="px-10 py-16 w-96 shadow-lg border-2 border-red-400 bg-gradient-to-br from-red-500 via-red-600 to-orange-600"
+        >
           <BsBarChartLine className="text-white h-10 w-12 mb-12" />
-          <p className="font-bold text-3xl text-white mb-1">Future Vision.</p>
-          <hr className="w-12 text-white border-[1px] mb-6" />
+          <motion.p
+            initial={{ opacity: 0, x: -15 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.3, duration: 0.6 }}
+            className="font-bold text-3xl text-white mb-1"
+          >
+            Future Vision.
+          </motion.p>
+          <hr className="w-12 text-white border-white border-[1px] mb-6" />
 
-          <p className="text-sm text-white">
+          <motion.p
+            initial={{ opacity: 0, x: -15 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.3, duration: 0.6 }}
+            className="text-sm text-white"
+          >
             It is a long established fact that a reader will be distracted by
             the readable content of a page.
-          </p>
-        </div>
-        <div className="px-10 py-16 w-96 text-zinc-900 hover:text-white bg-white hover:bg-gradient-to-br from-red-500 via-red-600 to-orange-600">
-          <BsBarChartLine className="text-red-500 h-10 w-12 mb-12" />
-          <p className="font-bold text-3xl mb-1">Future Vision.</p>
-          <hr className="w-12 border-[1px] mb-6" />
+          </motion.p>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.7 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ delay: 0, duration: 0.5 }}
+          className="group px-10 py-16 w-96 shadow-lg border-2 border-zinc-100 hover:border-red-400 bg-gradient-to-br from-white to-zinc-50 hover:bg-gradient-to-br hover:from-red-500 hover:via-red-600 hover:to-orange-600 "
+        >
+          <MdOutlineRocketLaunch className="text-red-500 group-hover:text-white h-10 w-12 mb-12" />
+          <motion.p
+            initial={{ opacity: 0, x: -15 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.3, duration: 0.6 }}
+            className="font-bold text-3xl text-zinc-900  group-hover:text-white mb-1"
+          >
+            Future Vision.
+          </motion.p>
+          <hr className="w-12 border-red-500 border-[1px] group-hover:border-white mb-6" />
 
-          <p className="text-sm">
+          <motion.p
+            initial={{ opacity: 0, x: -15 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.3, duration: 0.6 }}
+            className="text-sm text-zinc-900 group-hover:text-white "
+          >
             It is a long established fact that a reader will be distracted by
             the readable content of a page.
-          </p>
-        </div>
+          </motion.p>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.7 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ delay: 0, duration: 0.5 }}
+          className="group px-10 py-16 w-96 shadow-lg border-2 border-zinc-100 hover:border-red-400 bg-gradient-to-br from-white to-zinc-50 hover:bg-gradient-to-br hover:from-red-500 hover:via-red-600 hover:to-orange-600 "
+        >
+          <AiOutlinePieChart className="text-red-500 group-hover:text-white h-10 w-12 mb-12" />
+          <motion.p
+            initial={{ opacity: 0, x: -15 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.3, duration: 0.6 }}
+            className="font-bold text-3xl text-zinc-900  group-hover:text-white mb-1"
+          >
+            Future Vision.
+          </motion.p>
+          <hr className="w-12 border-red-500 border-[1px] group-hover:border-white mb-6" />
+
+          <motion.p
+            initial={{ opacity: 0, x: -15 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.3, duration: 0.6 }}
+            className="text-sm text-zinc-900 group-hover:text-white "
+          >
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page.
+          </motion.p>
+          <div className="w-96 h-16 bg-red-500 absolute translate-y-1/2 right-2 lg:right-8 2xl:right-16 -z-10"></div>
+        </motion.div>
       </div>
     </div>
   );
