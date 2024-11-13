2f43a9d7501009da72129071e6a02862cb6e2fa5
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-11-13 19:26:47 +0500

    updated

--- portfolio-web/src/components/cardsHome.tsx
+++ portfolio-web/src/components/cardsHome.tsx
@@ -4,6 +4,7 @@ import { TbUserStar } from "react-icons/tb";
 import { PiShootingStar } from "react-icons/pi";
 import { IoColorPaletteOutline } from "react-icons/io5";
 import { LuCalendarHeart } from "react-icons/lu";
+import { MdOutlineDesignServices } from "react-icons/md";
 import { FaChartLine } from "react-icons/fa";
 import CountUp from "react-countup";
 import { useState } from "react";
@@ -22,7 +23,7 @@ const portfolioStats = [
   },
   {
     number: 150,
-    icon: <IoColorPaletteOutline />,
+    icon: <MdOutlineDesignServices />,
     heading: "Designs Created",
   },
   {
@@ -49,9 +50,9 @@ export default function CardsHome() {
             whileInView={{ opacity: 1, scale: 1 }}
             transition={{ delay: 0, duration: 0.5 }}
             key={index}
-            className="group relative flex flex-col items-center justify-center gap-1 w-44 pt-11 pb-6 rounded-xl border-b-4 border-b-red-700 bg-gradient-to-br from-red-500 via-red-600 to-orange-600 shadow-lg"
+            className="group relative flex flex-col items-center justify-center gap-1 w-44 pt-11 pb-6 rounded-xl border-b-4 border-b-red-700 group-hover:border-zinc-300 bg-gradient-to-br from-red-500 via-red-600 to-orange-600 shadow-lg hover:bg-gradient-to-br hover:from-white hover:to-zinc-100"
           >
-            <span className="text-red-500 shadow-md border-2 border-zinc-200 text-3xl absolute bg-white p-[14px] rounded-full top-0 -translate-y-1/2">
+            <span className="text-red-500 group-hover:text-white shadow-md border-2 border-zinc-200 group-hover:border-red-400 text-3xl absolute bg-white group-hover:bg-red-500 p-[14px] rounded-full top-0 -translate-y-1/2">
               {item.icon}
             </span>
             <ScrollTrigger
@@ -61,9 +62,17 @@ export default function CardsHome() {
               {onScreen && (
                 <CountUp start={0} end={item.number} duration={5}>
                   {({ countUpRef }) => (
-                    <h4 className="text-white text-4xl font-bold">
+                    <h4 className="text-white group-hover:text-zinc-900 text-4xl font-bold flex justify-center items-center gap-1">
                       <span ref={countUpRef} />
-                      {index === 0 || index === 1 || index === 2 ? "+" : "%"}
+                      <span
+                        className={
+                          index === 0 || index === 1 || index === 2
+                            ? "text-3xl"
+                            : "text-2xl"
+                        }
+                      >
+                        {index === 0 || index === 1 || index === 2 ? "+" : "%"}
+                      </span>
                     </h4>
                   )}
                 </CountUp>
