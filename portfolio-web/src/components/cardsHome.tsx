e12fcee3c43321deeab9ab37c1d26d078dfeb066
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-11-14 10:19:16 +0500

    updated

--- portfolio-web/src/components/cardsHome.tsx
+++ portfolio-web/src/components/cardsHome.tsx
@@ -6,7 +6,6 @@ import { LuCalendarHeart } from "react-icons/lu";
 import { MdOutlineDesignServices } from "react-icons/md";
 import CountUp from "react-countup";
 import { useState } from "react";
-import ScrollTrigger from "react-scroll-trigger";
 
 const portfolioStats = [
   {
@@ -37,51 +36,54 @@ const portfolioStats = [
 ];
 
 export default function CardsHome() {
-  let [onScreen, setOnScreen] = useState(false);
+  const [hasEntered, setHasEntered] = useState(false);
 
   return (
-    <div>
-      <div className="flex justify-center items-center gap-6 my-24">
-        {portfolioStats.map((item, index) => (
-          <motion.div
-            initial={{ opacity: 0, scale: 0.7 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            transition={{ delay: 0, duration: 0.5 }}
-            key={index}
-            className="group relative flex flex-col items-center justify-center gap-1 w-44 pt-11 pb-6 rounded-xl border-b-4 border-b-red-700 group-hover:border-zinc-300 bg-gradient-to-br from-red-500 via-red-600 to-orange-600 hover:bg-gradient-to-br hover:from-white hover:to-zinc-100 shadow-md"
-          >
-            <span className="text-red-500 group-hover:text-white shadow-md border-2 border-zinc-200 group-hover:border-red-400 text-3xl absolute bg-gradient-to-br from-white to-zinc-50 group-hover:bg-gradient-to-br group-hover:from-red-500 group-hover:via-red-600 group-hover:to-orange-600 p-[14px] rounded-full top-0 -translate-y-1/2">
-              {item.icon}
-            </span>
-            <ScrollTrigger
-              onEnter={() => setOnScreen(true)}
-              onExit={() => setOnScreen(false)}
+    <div className="flex justify-center items-center gap-6 my-24">
+      {portfolioStats.map((item, index) => (
+        <motion.div
+          key={index}
+          initial={{ opacity: 0, scale: 0.7 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="group relative flex flex-col items-center justify-center gap-1 w-44 pt-11 pb-6 rounded-xl border-b-4 border-b-red-700 group-hover:border-zinc-300 bg-gradient-to-br from-red-500 via-red-600 to-orange-600 hover:bg-gradient-to-br hover:from-white hover:to-zinc-100 shadow-md"
+          onViewportEnter={() => setHasEntered(true)}
+          onViewportLeave={() => setHasEntered(false)}
+        >
+          <span className="text-red-500 group-hover:text-white shadow-md border-2 border-zinc-200 group-hover:border-red-400 text-3xl absolute bg-gradient-to-br from-white to-zinc-50 group-hover:bg-gradient-to-br group-hover:from-red-500 group-hover:via-red-600 group-hover:to-orange-600 p-[14px] rounded-full top-0 -translate-y-1/2">
+            {item.icon}
+          </span>
+
+          {/* Use key to force reset and rerender CountUp */}
+          {hasEntered && (
+            <CountUp
+              key={item.number} // Changing the key ensures that CountUp restarts each time it's in view
+              start={1}
+              end={item.number}
+              duration={5}
             >
-              {onScreen && (
-                <CountUp start={0} end={item.number} duration={5}>
-                  {({ countUpRef }) => (
-                    <h4 className="text-white group-hover:text-zinc-900 text-4xl font-bold flex justify-center items-center gap-1">
-                      <span ref={countUpRef} />
-                      <span
-                        className={
-                          index === 0 || index === 1 || index === 2
-                            ? "text-3xl"
-                            : "text-2xl"
-                        }
-                      >
-                        {index === 0 || index === 1 || index === 2 ? "+" : "%"}
-                      </span>
-                    </h4>
-                  )}
-                </CountUp>
+              {({ countUpRef }) => (
+                <h4 className="text-white group-hover:text-zinc-900 text-4xl font-bold flex justify-center items-center gap-1">
+                  <span ref={countUpRef} />
+                  <span
+                    className={
+                      index === 0 || index === 1 || index === 2
+                        ? "text-3xl"
+                        : "text-2xl"
+                    }
+                  >
+                    {index === 0 || index === 1 || index === 2 ? "+" : "%"}
+                  </span>
+                </h4>
               )}
-            </ScrollTrigger>
-            <p className="text-white group-hover:text-red-500 text-xs font-medium leading-none">
-              {item.heading}
-            </p>
-          </motion.div>
-        ))}
-      </div>
+            </CountUp>
+          )}
+
+          <p className="text-white group-hover:text-red-500 text-xs font-medium leading-none">
+            {item.heading}
+          </p>
+        </motion.div>
+      ))}
     </div>
   );
 }
