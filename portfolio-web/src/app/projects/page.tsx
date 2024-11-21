b4a07d62e131edb5a94af96264a2fa7b2005a369
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2024-11-21 10:09:23 +0500

    updated

--- portfolio-web/src/app/projects/page.tsx
+++ portfolio-web/src/app/projects/page.tsx
@@ -11,12 +11,24 @@ let Project = [
   { name: "Art", delay: 0.5 },
 ];
 
+const projectData = [
+  { title: "Branding Project", category: "Branding", src: "" },
+  { title: "Creative Project", category: "Creative", src: "" },
+  { title: "Design Project", category: "Design", src: "" },
+  { title: "Art Project", category: "Art", src: "" },
+  { title: "Another Art Project", category: "Art", src: "" },
+];
 export default function Projects() {
   const [active, setActive] = useState("All Projects");
 
   const isActive = (item: string) => {
     setActive(item);
   };
+
+  const filterProjects =
+    active === "All Projects"
+      ? projectData
+      : projectData.filter((e) => e.category === active);
 
   return (
     <div className="px-6 lg:px-12 2xl:px-24 relative py-12">
@@ -51,6 +63,28 @@ export default function Projects() {
           >
             {itemName.name}
           </motion.p>
+        ))}
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {filterProjects.map((e, i) => (
+          <motion.div
+            key={i}
+            initial={{ opacity: 0, scale: 0.8 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.6 }}
+            whileHover={{
+              scale: 1.05,
+              transition: { duration: 0.3 },
+            }}
+            whileTap={{
+              scale: 0.95,
+              transition: { duration: 0.1 },
+            }}
+            className="bg-red-500"
+          >
+            <h4>{e.title}</h4>
+            <p>{e.category}</p>
+          </motion.div>
         ))}
       </div>
     </div>
